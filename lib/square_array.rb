d16c59b2a8abcd6ee58f782364b97b075ddfae07
Stef Spears <spears.sj87@gmail.com>
2019-07-10 20:49:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
+all_squared = []
 def square_array(array)
   counter = 0 
-  while array
+  while counter < array.length do
+    p all_squared.push(array[counter] * array[counter])
+    counter += 1 
+  end
+  
