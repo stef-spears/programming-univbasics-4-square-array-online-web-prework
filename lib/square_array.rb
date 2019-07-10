52d9a999262ce0ebaea0946e63a079092bd206a5
Stef Spears <spears.sj87@gmail.com>
2019-07-10 21:16:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   squared = []
   counter = 0 
   while counter < array.length do
-    squared.push(array[counter]*array[counter])
+    squared << array[counter]**2
     counter += 1 
   end
   p squared
